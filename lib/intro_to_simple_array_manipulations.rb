4b7d09fe9ac925058d658f940507da2d95f8bca0
Jeff Fisk <jeff.fisk512@gmail.com>
2020-02-27 22:20:23 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,41 @@
+
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop() 
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  
+end
+
+def shift_with_args(array)
+  
+end
+
+def using_concat(array1, array2)
+  
+end
+
+def using_insert(array, element)
+  
+end
+
+def using_
+  
+end
+
+def 
+  
+end
+
